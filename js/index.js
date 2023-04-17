6891d588d1423f78863248ede007176e3b05f267
MariaParada8 <130449966+MariaParada8@users.noreply.github.com>
2023-04-17 20:21:30 +0200

    Sauvegardeee

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
-const toggleMenu = document.querySelector("#toggle-menu");
-const menu = document.querySelector("#menu");
+const buttons = document.querySelectorAll('.menu-btn');
+const boxes = document.querySelectorAll('.box');
 
-toggleMenu.addEventListener("click", () => {
-  menu.classList.toggle("show-menu");
+buttons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const target = button.dataset.target;
+    boxes.forEach((box) => {
+      if (box.getAttribute('id') === target) {
+        box.classList.add('active');
+      } else {
+        box.classList.remove('active');
+      }
+    });
+    buttons.forEach((btn) => {
+      if (btn === button) {
+        btn.classList.add('active');
+      } else {
+        btn.classList.remove('active');
+      }
+    });
+  });
 });
