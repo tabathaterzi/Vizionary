696bedceedf9ca48f456f73410fecb80ecbeec7c
MariaParada8 <130449966+MariaParada8@users.noreply.github.com>
2023-05-21 17:10:16 +0200

    sauvegardee

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+
+
+
+// Obtén referencias a los elementos del DOM
 const buttons = document.querySelectorAll('.menu-btn');
 const boxes = document.querySelectorAll('.box');
+const formatoElegidoContainer = document.getElementById("visualisation-container");
+const fileInput = document.querySelector(".file-input");
+const chooseImgBtn = document.querySelector(".choose-img");
 
+// Agrega los event listeners a los botones del menú
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const target = button.dataset.target;
@@ -21,71 +29,94 @@ buttons.forEach((button) => {
   });
 });
 
+// Event listener para seleccionar una imagen
+const chooseImage = () => {
+  fileInput.click();
+};
 
-/*
-  const visualisationDiv = document.getElementById('visualisation');
-  const formatBtns = document.querySelectorAll('.format-btn');
-  
-  formatBtns.forEach(btn => {
-    btn.addEventListener('click', () => {
-      visualisationDiv.innerHTML = `<img src="./img/Format 16_9.svg" alt="visualisation_format ${btn.dataset.format}" width="100%">`;
-    });
-  });
-
-*/
-
-
-
-
-  const exportBtn = document.querySelector('.export-btn');
-  exportBtn.addEventListener('click', () => {
-    // exporter l'image ici
-  });
-
-
-
-// Récupère la div format-choisi de la box container 2
-const formatChoisi = document.querySelector('.box-container_2 .format-choisi');
-
-// Récupère tous les boutons de format
-const boutonsFormats = document.querySelectorAll('#formats .format');
-
-
-// Parcours tous les boutons de format
-boutonsFormats.forEach(boutonFormat => {
-  // Ecoute l'événement de clic sur chaque bouton de format
-  boutonFormat.addEventListener('click', () => {
-    // Récupère la largeur et la hauteur du format sélectionné
-    const width = boutonFormat.dataset.width;
-    const height = boutonFormat.dataset.height;
-
-    // Met à jour le contenu de la div format-choisi avec le format sélectionné
-    formatChoisi.innerHTML = `
-    <img class="small ${width === '16' && height === '9' ? 'landscape' : 'portrait'}" src="./img/Format ${width}_${height}.svg" alt="" width="100%">
-    `;
-  });
-})
-
-
-
-
-
-
-
-
-const colorPicker = document.getElementById('color-picker');
-const addColorBtn = document.getElementById('add-color-btn');
-const colorList = document.getElementById('color-list');
-
-addColorBtn.addEventListener('click', () => {
-  const color = colorPicker.value;
-  const li = document.createElement('li');
-  const span = document.createElement('span');
-  span.textContent = color;
-  li.appendChild(span);
-  li.style.backgroundColor = color;
-  li.classList.add('color-item');
-  colorList.appendChild(li);
+fileInput.addEventListener("change", () => {
+  const selectedFile = fileInput.files[0];
+  // Aquí puedes realizar las acciones necesarias con el archivo seleccionado
 });
 
+chooseImgBtn.addEventListener("click", chooseImage);
 
+// Función para mostrar un modelo seleccionado
+function showModel(modelId) {
+  // Obtén una referencia al modelo seleccionado
+  var modeloSeleccionado = document.getElementById(modelId);
+
+  // Elimina cualquier modelo clonado previo en el contenedor del formato elegido
+  while (formatoElegidoContainer.firstChild) {
+    formatoElegidoContainer.firstChild.remove();
+  }
+
+  // Clona el modelo seleccionado y agrega la clase "modelo-clonado" para estilizarlo
+  var modeloClonado = modeloSeleccionado.cloneNode(true);
+  modeloClonado.classList.add("modelo-clonado");
+
+  // Agrega el modelo clonado al contenedor del formato elegido
+  formatoElegidoContainer.appendChild(modeloClonado);
+}
+
+
+function showFormat(formatId) {
+  // Obtén una referencia al formato seleccionado
+  var formatoSeleccionado = document.getElementById(formatId);
+
+  // Elimina cualquier formato clonado previo en el contenedor del formato elegido
+  while (formatoElegidoContainer.firstChild) {
+    formatoElegidoContainer.firstChild.remove();
+  }
+
+  // Clona el formato seleccionado y agrega la clase "formato-clonado" para estilizarlo
+  var formatoClonado = formatoSeleccionado.cloneNode(true);
+  formatoClonado.classList.add("formato-clonado");
+
+  // Agrega el formato clonado al contenedor del formato elegido
+  formatoElegidoContainer.appendChild(formatoClonado);
+}
+
+document.getElementById('format1').addEventListener('click', function() {
+  var formatId = this.getAttribute('id');
+  showFormat(formatId);
+});
+
+// Función para mostrar un modelo seleccionado
+function showModel(modelId) {
+  // Obtén una referencia al modelo seleccionado
+  const modeloSeleccionado = document.getElementById(modelId);
+
+  // Elimina cualquier modelo clonado previo en el contenedor del formato elegido
+  while (formatoElegidoContainer.firstChild) {
+    formatoElegidoContainer.firstChild.remove();
+  }
+
+  // Clona el modelo seleccionado y agrega la clase "modelo-clonado" para estilizarlo
+  const modeloClonado = modeloSeleccionado.cloneNode(true);
+  modeloClonado.classList.add("modelo-clonado");
+
+  // Agrega el modelo clonado al contenedor del formato elegido
+  formatoElegidoContainer.appendChild(modeloClonado);
+}
+
+
+
+
+// Ejemplo de modificación del texto
+var elementoTexto = document.getElementById("modèle1");
+elementoTexto.addEventListener("input", function(event) {
+  var nuevoTexto = event.target.value; // Obtén el nuevo texto del campo de entrada
+  // Actualiza el texto en el elemento correspondiente
+  elementoTexto.textContent = nuevoTexto;
+});
+
+// Ejemplo de cambio de imagen
+var elementoImagen = document.getElementById("modèle");
+var campoCarga = document.getElementById("id-del-campo-de-carga-de-imagen");
+campoCarga.addEventListener("change", function(event) {
+  var nuevaImagen = event.target.files[0]; // Obtén la imagen cargada
+  var nuevaURL = URL.createObjectURL(nuevaImagen); // Crea una URL para la nueva imagen
+  // Actualiza el atributo src de la etiqueta <img> con la nueva URL de la imagen
+  elementoImagen.src = nuevaURL;
+});
