2259171edfcd92eff77a8da022f1583b9dc9d90a
MariaParada8 <130449966+MariaParada8@users.noreply.github.com>
2023-05-22 13:56:42 +0200

    sauvegarde

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 
 
-
+let GlobalModelId = ""
 // Obtén referencias a los elementos del DOM
 const buttons = document.querySelectorAll('.menu-btn');
 const boxes = document.querySelectorAll('.box');
@@ -29,22 +29,25 @@ buttons.forEach((button) => {
   });
 });
 
-// Event listener para seleccionar una imagen
-const chooseImage = () => {
-  fileInput.click();
-};
 
-fileInput.addEventListener("change", () => {
-  const selectedFile = fileInput.files[0];
-  // Aquí puedes realizar las acciones necesarias con el archivo seleccionado
-});
 
-chooseImgBtn.addEventListener("click", chooseImage);
+
+
+
+
 
 // Función para mostrar un modelo seleccionado
 function showModel(modelId) {
+
+  let modele = GlobalModelId 
+  
+  if(modelId){
+    modele = modelId
+    GlobalModelId = modelId
+  }
+  console.log(modelId,GlobalModelId,modele)
   // Obtén una referencia al modelo seleccionado
-  var modeloSeleccionado = document.getElementById(modelId);
+  var modeloSeleccionado = document.getElementById(modele);
 
   // Elimina cualquier modelo clonado previo en el contenedor del formato elegido
   while (formatoElegidoContainer.firstChild) {
@@ -77,28 +80,53 @@ function showFormat(formatId) {
   formatoElegidoContainer.appendChild(formatoClonado);
 }
 
-document.getElementById('format1').addEventListener('click', function() {
-  var formatId = this.getAttribute('id');
-  showFormat(formatId);
-});
 
-// Función para mostrar un modelo seleccionado
-function showModel(modelId) {
-  // Obtén una referencia al modelo seleccionado
-  const modeloSeleccionado = document.getElementById(modelId);
 
-  // Elimina cualquier modelo clonado previo en el contenedor del formato elegido
-  while (formatoElegidoContainer.firstChild) {
-    formatoElegidoContainer.firstChild.remove();
-  }
-
-  // Clona el modelo seleccionado y agrega la clase "modelo-clonado" para estilizarlo
-  const modeloClonado = modeloSeleccionado.cloneNode(true);
-  modeloClonado.classList.add("modelo-clonado");
-
-  // Agrega el modelo clonado al contenedor del formato elegido
-  formatoElegidoContainer.appendChild(modeloClonado);
+function afficher () {
+  var titre = document.getElementById("input_titre").value;
+  console.log("Le titre est : " + titre);
 }
 
 
+// Récupérer l'élément input_titre
+const inputTitre = document.getElementById('input_titre');
+
+// Ajouter un écouteur d'événements onchange
+inputTitre.addEventListener('keyup', function(event) {
+  // Récupérer la valeur du titre saisi par l'utilisateur
+  const nouveauTitre = event.target.value;
+
+  // Mettre à jour le titre dans la visualisation
+  const titreModele = document.querySelector('.titre_modèle1');
+  titreModele.textContent = nouveauTitre;
+
+  showModel()
+});
+
+
+
+
+
+
+
+// Récupérer l'élément input_image
+const inputImage = document.getElementById('input_image');
+
+// Ajouter un écouteur d'événements change
+inputImage.addEventListener('change', function(event) {
+  // Vérifier si un fichier a été sélectionné
+  if (event.target.files && event.target.files[0]) {
+    const fichier = event.target.files[0];
+    
+    // Créer un objet URL pour le fichier sélectionné
+    const urlImage = URL.createObjectURL(fichier);
+
+    // Mettre à jour la source de l'image dans la visualisation
+    const imageModele = document.getElementById('image_modele');
+    imageModele.src = urlImage;
+  }
+
+  showModel()
+
+});
 
